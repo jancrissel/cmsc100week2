e9d019e56a3193175678724280c05902147ae0a6
jancrissel <joderamos@up.edu.ph>
2015-08-26 12:22:59 +0800

    added files

--- config/router.js
+++ config/router.js
@@ -1,14 +1,20 @@
 var student = require('./../controllers/student');
-var teacher = require('./../controllers/teacher');
+//var teacher = require('./../controllers/teacher');
 
 module.exports = function(router){
        router.route('/students')
-       .get(student.find);
-       
+       .get(student.find)
+       .post(student.insert);
+       router.route('/students/:id')
+       .get(student.findOne)
+       .put(student.update)
+       .delete(student.remove);   
+    
        return router;
 
 };
 
+/*
 module.exports = function(router){
        router.route('/teachers')
        .get(teacher.find)
@@ -16,4 +22,4 @@ module.exports = function(router){
        
        return router;
 
-};
+};*/
